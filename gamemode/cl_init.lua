20170a6881335526bce14587929b0433baca5663
Bungos <me.bungo@gmail.com>
2019-06-10 22:13:04 +0100

    Create cl_init.lua

---
+++ gamemode/cl_init.lua
@@ -0,0 +1,16 @@
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "shared.lua" )
+
+include( "shared.lua" )
+
+
+function GM:PlayerConnect(name, ip)
+
+
+
+end
+
+function GM:PlayerInitialSpawn(ply)
+
+
+end
