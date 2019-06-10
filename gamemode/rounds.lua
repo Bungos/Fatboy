38caa6720e45d5ee0e77dcef2d03d3a2b451c553
Bungos <me.bungo@gmail.com>
2019-06-10 22:12:28 +0100

    Create rounds.lua

---
+++ gamemode/rounds.lua
@@ -0,0 +1,63 @@
+
+function RoundStart()
+	local Alive = 0 
+	for k, v in pairs( player.GetAll() ) do
+		if (v:Alive() ) then
+			Alive = Alive + 1
+		end
+	end
+		if ( Alive >= table.Count( player.GetAll() ) && table.Count( player.GetAll() ) > 1 ) then
+			roundActive = true
+		end
+		print( "Round started " .. tostring(roundActive))
+
+		RoundEndCheck()
+	end
+
+	function RoundEndCheck()
+		print ( "Round active " .. tostring (roundActive))
+		if(roundActive == false ) then return end
+		timer.Create( "checkdelay", 1, 1, function()
+			local FBAlive = 0
+			local SAlive = 0
+			for k, v in pairs( team.GetPlayers(0) ) do
+				if( v:Alive() ) then
+					FBAlive = FBAlive + 1
+				end
+			end
+
+			for k, v in pairs( team.GetPlayers(1) ) do
+				if( v:Alive() ) then
+					SAlive = SAlive + 1
+				end
+			end
+
+
+			if (FBAlive == 0 ) then
+				EndRound("Survivor")
+			elseif (SAlive == 0 ) then
+				EndRound("Fatboy")
+			end
+
+		end)
+	end
+
+	function EndRound( winners )
+
+		print ( winners .. " won the round!")
+	timer.Create( "cleanup", 3, 1, function ()
+		game.CleanUpMap( false, {} )
+		for _, v in pairs( player.GetAll() ) do
+		if ( 	v:Alive() ) then
+				v:SetupHands()
+				v:StripWeapons()
+				v:KillSilent()
+		end			
+					v:SetupTeam ( v:Team() )
+		
+	end
+	roundActive = false
+
+
+	end)
+end
